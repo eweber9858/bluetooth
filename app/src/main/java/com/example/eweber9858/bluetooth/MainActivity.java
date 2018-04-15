d98bb9293ee21dc6551ecc8e692d76ed55ee7f8a
eweber9858 <eweber9858@eweber9858s-MacBook-Pro.local>
2018-04-15 15:52:21 -0500

    added button

--- app/src/main/java/com/example/eweber9858/bluetooth/MainActivity.java
+++ app/src/main/java/com/example/eweber9858/bluetooth/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.eweber9858.bluetooth;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Switch simpleSwitch = (Switch) findViewById(R.id.bluetoothSwitch);
+        if(simpleSwitch.isChecked()) {
+            //add code to poll for bluetooth signal here
+        }
     }
+
 }
